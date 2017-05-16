9b6046afe64b7259b141281c368d9c3020f175e1
Ramraj Sekar <E002040@cigniti.com>
2017-05-16 10:50:48 +0530

    This is from office

---
+++ LearningBasics.R
@@ -0,0 +1,40 @@
+a <- "Hello world"
+print(a)
+
+v <- TRUE
+print(class(v))
+
+v <- apple
+apple <- c('red','green',"yellow")
+print(apple)
+print(class(apple))
+
+print(2+3)
+
+print(6/3)
+
+print((3*8)/(2*3))
+
+print(log(15))
+
+print(sqrt(121))
+
+x = 5+95
+print(x)
+
+s1="test"
+s2 = "rest"
+
+print(paste(s1, s2, sep = " "))
+
+#TO find length of string
+a = nchar(s1)
+print(paste("The length of string s1 is ",a))
+
+
+b = c(1.8,4.5)
+print(class(b))
+
+
+e = vector("logical", length = 5)
+print(class(e))
