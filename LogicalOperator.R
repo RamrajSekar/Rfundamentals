738d54e14b2f250ae08d4d5f6cb930444902bb11
RamrajSekar <sekar.ramraj@gmail.com>
2017-05-19 12:59:46 +0530

    updated

--- LogicalOperator.R
+++ LogicalOperator.R
@@ -1,3 +1,27 @@
                           #LogicalOperator#
+  
+                          #& operator
 
-test
+#Only TRUE if both are True
+TRUE & TRUE
+#False if either of the end is false
+TRUE & FALSE
+FALSE & TRUE
+FALSE & FALSE
+
+g <- 12
+
+g > 5 & g < 15
+
+                          #OR Operator
+#TRUE if atleast one true
+TRUE | TRUE
+TRUE | FALSE
+FALSE | FALSE
+
+                          #NOT Operator
+#
+!TRUE
+!FALSE
+!(g<5)
+g >=5
